837063b3d343a2968950abe3f4119db61c630b52
Korayckr <koraycakir1@hotmail.com>
2023-05-30 11:30:32 +0300

    Add project files.

---
+++ ConsoleAppKalitimOrnegi/Hayvan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppKalitimOrnegi
+{
+
+    // Hayvan ismi verilen sınıfta daha öncekilerde oldupu gibi methodlar ve değişkenler tanımlandı.
+    // Farklı olarak yaş nesnesinin erişim belirleyicisi private olarak tanımlandı.
+    public class Hayvan
+    {
+        public string hayvanName;
+        public string hayvanGender;
+        private int hayvanYas;
+
+        public void hayvanInfo()
+        {
+            string hayvan = "Hayvanın İsmi: " + hayvanName + "Hayvanın Cinsiyeti: " + hayvanGender;
+                
+
+            Console.WriteLine(hayvan);
+
+
+        }
+
+    }
+}
+
+
---
+++ ConsoleAppKalitimOrnegi/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+
+namespace ConsoleAppKalitimOrnegi
+{
+    class program{
+
+        static void Main(string[] args)
+        {
+
+            // Kalıtılan sınıf üzerinden işlemler gerçeklerştirildi.
+            Kedi kedi = new Kedi();
+
+            kedi.hayvanName = "Mia \n";
+            kedi.hayvanGender = "Dişi \n";
+
+
+            kedi.hayvanInfo();
+
+            Console.ReadKey();
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+
+
+}
